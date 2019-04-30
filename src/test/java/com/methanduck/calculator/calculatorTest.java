91ea3d86e60be2f757304ea41c5a8bcd45b2cdc1
student <dhdms6512@gmail.com>
2019-04-30 14:04:52 +0900

    update

--- src/test/java/com/methanduck/calculator/calculatorTest.java
+++ src/test/java/com/methanduck/calculator/calculatorTest.java
@@ -1,6 +1,9 @@
 package com.methanduck.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+/**
+* helllo
+*/
 public class calculatorTest {
 	private calculator Calculator = new calculator();
 

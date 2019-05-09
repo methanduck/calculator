2a2e9e75dfde6efa917d5710b35beba0a1d2a37c
student <dhdms6512@gmail.com>
2019-05-09 14:29:31 +0900

    update

--- accept.sh
+++ accept.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-test $(curl localhost:8090/sum?a=1\&b=2) -eq 3 || \
-test $(curl $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}'
-bridge):8090/sum?a=1\&b=2) –eq 3
+#test $(curl localhost:8090/sum?a=1\&b=2) -eq 3 || \
+#test $(curl $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}'bridge):8090/sum?=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose port calc 8090 | cut -d: -f2)
+test curl -X GET localhost:$CALCULATOR_PORT/sum?a=10\&b=2) -eq 12
+
